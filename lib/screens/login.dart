cfec3cd797e4deeda2facc86806f646fce12d2a6
vijay <vijay@websiteduniya.com>
2023-06-29 18:28:58 +0530

    some_changes_are_Done

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:videocall/screens/home.page.dart';
+import 'package:videocall/screens/otp.login.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     final width= MediaQuery.of(context).size.width;
     final height= MediaQuery.of(context).size.height;
-
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -17,40 +15,72 @@ class Login extends StatelessWidget {
         elevation: 0.0,
       ),
       body: SingleChildScrollView(
-        padding: const EdgeInsets.all(10),
+        physics: const BouncingScrollPhysics(),
+        padding: const EdgeInsets.all(15),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisSize: MainAxisSize.max,
           children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                hintText: "Number",
+            SizedBox(
+              width: width,
+              height: height * .4,
+              child: Image.asset("assets/images/logo.png"),
+            ),
+            SizedBox(height: height * .04),
+            const Text("Login to Continue",style: TextStyle(
+              fontSize: 25.0,fontWeight: FontWeight.bold,letterSpacing: 1,wordSpacing: 2,
+            ),),
+            SizedBox(height: height * .04,),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Color(0xffe94057),
+                      spreadRadius: 1,
+                      blurRadius: 10,
+                    )
+                  ]
+              ),
+              child: TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.only(left: width * .07),
+                  hintText: "Mobile No.",
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0)),
+                ),
               ),
             ),
-            SizedBox(height: height * .02),
-            TextFormField(
-              decoration: const InputDecoration(
-                hintText: "password",
-              ),
-            ),
-            SizedBox(height: height * .02),
+            SizedBox(height: height * .04,),
             InkWell(
               onTap: (){
                 Navigator.of(context).push(
-                  MaterialPageRoute(builder: (c)=>const HomePage()),
+                  MaterialPageRoute(builder: (c)=>const OtpLoginPage()),
                 );
               },
               child: Container(
                 alignment: Alignment.center,
-                height: height * .06,
                 width: width,
-                color: const Color(0xffE94057),
-                child: const Text("Continue",style: TextStyle(
-                  color: Colors.white,fontSize: 20.0,
+                height: height * .07,
+                decoration: BoxDecoration(
+                  color: const Color(0xffe94057),
+                  borderRadius: BorderRadius.circular(30),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Color(0xffe94057),
+                        spreadRadius: 1,
+                        blurRadius: 1,
+                      )
+                    ]
+                ),
+                child: const Text("Login",style: TextStyle(
+                  fontSize: 25.0,fontWeight: FontWeight.bold,color: Colors.white,
                 ),),
               ),
-            )
+            ),
+            SizedBox(height: height * .02,),
           ],
         ),
       ),
---
+++ lib/screens/otp.login.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
+import 'package:videocall/screens/home.page.dart';
+
+
+class OtpLoginPage extends StatelessWidget {
+  const OtpLoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final width= MediaQuery.of(context).size.width;
+    final height= MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top + height * .16,left: 15,right: 15,bottom: 15),
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Align(child: Container(
+              padding: const EdgeInsets.all(20),
+              height: height * .6,
+              width: width,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  const Align(
+                    child: Text('Verification Code',style: TextStyle(
+                      color: Colors.white,fontWeight: FontWeight.bold,fontSize: 30.0,
+                    ))),
+                  Container(
+                    alignment: Alignment.center,
+                    child: const Text("we have a verification code to your mobile \nnumber +91(9090909090)",style: TextStyle(
+                      color: Colors.white,fontSize: 16.0,
+                    )),
+                  ),
+                  Pinput(
+                    length: 6,
+                    defaultPinTheme: PinTheme(
+                      textStyle: const TextStyle(
+                        fontSize: 30.0,
+                      ),
+                      width: width * .2,
+                      height: height * .07,
+                      decoration: BoxDecoration(
+                        color: const Color(0xfff6f8fa),
+                        border: Border.all(color: const Color.fromRGBO(234, 239, 243, 1)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: (){
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (c)=>const HomePage()));
+                    },
+                    child: Container(
+                      height: height * .06,
+                      width: width,
+                      decoration: BoxDecoration(
+                        color: const Color(0xffe94057),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      alignment: Alignment.center,
+                      child: const Text("Continue",style: TextStyle(
+                        color: Colors.white,fontSize: 20.0,
+                      ),),
+                    ),
+                  )
+                ],
+              ),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
